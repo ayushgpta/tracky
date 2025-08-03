dfbcb59179cda33e783aef68ed91e35aab0900ba
ayushgpta <ayushdoescoding@gmail.com>
2025-08-03 12:48:07 +0530

    bug fixes

--- client/src/features/auth/authSlice.js
+++ client/src/features/auth/authSlice.js
@@ -1,8 +1,7 @@
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 import axios from 'axios';
-import api from './api.js';
 
-const API_URL = 'http://https://scheduly-backend-qpnp.onrender.com:5000/api';
+const API_URL = 'https://scheduly-backend-qpnp.onrender.com:5000/api';
 
 // Helper function to check if the stored token is valid
 export const isTokenValid = () => {
